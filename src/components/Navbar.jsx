8541cffdf40205a029e8b0f850daebe3961e48b6
Gosthavo <rosillomintic@gmail.com>
2026-05-31 18:03:52 -0500

    Fase 2. Login inicial

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,79 @@
 import { Link } from 'react-router-dom'
+import { useAuth } from '../context/AuthContext'
 
 function Navbar() {
+  const { user, setUser } = useAuth()
+
+  const logout = () => {
+    setUser(null)
+  }
+
   return (
     <nav className="bg-pink-500 text-white p-4 flex justify-between">
       <h1 className="text-2xl font-bold">
         🍨 FrostByte Ice Cream
       </h1>
 
-      <div className="flex gap-4">
+      <div className="flex gap-4 items-center">
         <Link to="/">Inicio</Link>
-        <Link to="/productos">Productos</Link>
-        <Link to="/ingredientes">Ingredientes</Link>
-        <Link to="/ventas">Ventas</Link>
-        <Link to="/login">Login</Link>
+
+        <Link to="/productos">
+          Productos
+        </Link>
+
+        {/* Público */}
+        {!user && (
+          <Link to="/login">
+            Login
+          </Link>
+        )}
+
+        {/* Cliente */}
+        {user?.rol === 'cliente' && (
+          <>
+            <Link to="/ventas">
+              Ventas
+            </Link>
+
+            <button onClick={logout}>
+              Salir
+            </button>
+          </>
+        )}
+
+        {/* Empleado */}
+        {user?.rol === 'empleado' && (
+          <>
+            <Link to="/ingredientes">
+              Ingredientes
+            </Link>
+
+            <Link to="/ventas">
+              Ventas
+            </Link>
+
+            <button onClick={logout}>
+              Salir
+            </button>
+          </>
+        )}
+
+        {/* Administrador */}
+        {user?.rol === 'admin' && (
+          <>
+            <Link to="/ingredientes">
+              Ingredientes
+            </Link>
+
+            <Link to="/ventas">
+              Ventas
+            </Link>
+
+            <button onClick={logout}>
+              Salir
+            </button>
+          </>
+        )}
       </div>
     </nav>
   )
